2e0511e96a614df54f1cd83e0813ef98579e933b
ZhouXiaoHong <Mars@hongzhoudeMacBook-Pro.local>
2015-09-07 22:09:30 +0800

    把另外一个文件找回来了

--- YiNian/Class/Tool/YNDBTool.swift
+++ YiNian/Class/Tool/YNDBTool.swift
@@ -5,32 +5,65 @@
 //  Created by 周宏 on 15/9/4.
 //  Copyright (c) 2015年 Mars. All rights reserved.
 //
-import SQLite
-import Foundation
+import UIKit
 
 class YNDBTool {
-    private static let db = SQLite.Database((NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String).stringByAppendingPathComponent("yinian.db"))
+    private static let path = (NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String).stringByAppendingPathComponent("yinian.db")
     
-    struct MessageDB {
-        static let table = db["nian"]
-        // 创建的日期
-        static let date = Expression<String>("date")
-        // 创建的时间 
-        static let dateStamp = Expression<NSTimeInterval>("dateStamp")
-        // 文本
-        static let text = Expression<String>("text")
-        // 图片路径
-        static let pic = Expression<String?>("pic")
-    }
-    
+    static let formatter: NSDateFormatter = {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyyMMdd"
+        return formatter
+        }()
     
     static func createTable() {
-        db.create(table: db["nian"], ifNotExists: true) { t in
-            println((NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String).stringByAppendingPathComponent("yinian.db"))
-            t.column(MessageDB.date, primaryKey: true)
-            t.column(MessageDB.dateStamp)
-            t.column(MessageDB.text)
-            t.column(MessageDB.pic)
+        let db = FMDatabase(path: path)
+        if db.open() {
+            println(path)
+            let sql = "CREATE TABLE IF NOT EXISTS nian (date TEXT PRIMARY KEY, dateStamp REAL, text TEXT, pic TEXT)"
+            db.executeStatements(sql)
+            db.close()
         }
+    }
+    
+    static func insertNian(nian: YNNian) {
+        let db = FMDatabase(path: path)
+        if db.open() {
+            let sql = "INSERT INTO nian VALUES (:date, :dateStamp, :text, :pic)"
+            
+            if let pic = nian.pic {
+                db.executeUpdate(sql, withParameterDictionary: ["date": formatter.stringFromDate(nian.date), "dateStamp":nian.date.timeIntervalSince1970, "text": nian.text, "pic": pic])
+            } else {
+                db.executeUpdate(sql, withParameterDictionary: ["date": formatter.stringFromDate(nian.date), "dateStamp":nian.date.timeIntervalSince1970, "text": nian.text, "pic": NSNull()])
+            }
+            
+            db.close()
+        }
+    }
+    
+    static func deleteTodayNian() {
+        let db = FMDatabase(path: path)
+        if db.open() {
+            let sql = "DELETE FROM nian WHERE date = ?"
+            db.executeUpdate(sql, formatter.stringFromDate(NSDate()))
+        }
+    }
+    
+    static func queryAllNian() -> Array<YNNian>? {
+        let db = FMDatabase(path: path)
+        if db.open() {
+            let sql = "SELECT * FROM nian"
+            let rs = db.executeQuery(sql)
+            if let rs = rs {
+                var array = Array<YNNian>()
+                while rs.next() {
+                    let nian = YNNian(date:NSDate(timeIntervalSince1970: (rs.doubleForColumn("dateStamp"))), text: rs.stringForColumn("text"), pic: rs.stringForColumn("pic"))
+                    array.append(nian)
+                }
+                return array
+            }
+            db.close()
+        }
+        return nil
     }
 }
